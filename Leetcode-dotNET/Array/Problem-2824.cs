94fa7a5cd70ed7feb1ed0e8c6797a3c03a2a54ac
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2024-01-27 21:13:49 +0500

    Problem-2824

---
+++ Leetcode-dotNET/Array/Problem-2824.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode_dotNET.Array;
+
+public class Problem_2824
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        int result = 0;
+        for(int i = 0;i < nums.Count; i++)
+        {
+            for(int j = i + 1; j < nums.Count; j++)
+            {
+                if (nums[i] + nums[j] < target)
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
