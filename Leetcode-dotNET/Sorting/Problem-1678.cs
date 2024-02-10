172c6e77356499fb61e71e2686aa5437459e90a2
javohirtohirovich <javoergashev30@gmail.com>
2024-02-10 14:49:32 +0500

    Slove problem 1678

---
+++ Leetcode-dotNET/Sorting/Problem-1678.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode_dotNET.Sorting;
+
+public class Problem_1678
+{
+    public string Interpret(string command)
+    {
+        command = command.Replace("()", "o");
+        command = command.Replace("(al)", "al");
+        return command;
+    }
+}
