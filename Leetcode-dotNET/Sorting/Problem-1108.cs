5d87c4f74a4cdb950ed484022b74da14a06aa19e
javohirtohirovich <javoergashev30@gmail.com>
2024-02-10 14:35:59 +0500

    Create Sorting Folder and Slove Problem-1108

---
+++ Leetcode-dotNET/Sorting/Problem-1108.cs
@@ -0,0 +1,31 @@
+﻿namespace Leetcode_dotNET.Sorting;
+
+public class Problem_1108
+{
+    //1-Method
+    /*public string DefangIPaddr(string address)
+    {
+        string result = "";
+        for(int i = 0;i<address.Length;i++)
+        {
+            if (address[i] == '.')
+            {
+                result += "[.]";
+            }
+            else
+            {
+                result += address[i];
+            }
+        }
+        return result;
+    }*/
+
+    //2-Method
+    public string DefangIPaddr(string address)
+    {
+        address = address.Replace(".", "[.]");
+        return address;
+    }
+    
+
+}
