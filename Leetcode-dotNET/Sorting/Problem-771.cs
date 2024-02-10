c65b5f3a73b5c2e76ceda2e98643cb18487cf173
javohirtohirovich <javoergashev30@gmail.com>
2024-02-10 14:45:14 +0500

    Slove Problem-771

---
+++ Leetcode-dotNET/Sorting/Problem-771.cs
@@ -0,0 +1,17 @@
+﻿namespace Leetcode_dotNET.Sorting;
+
+public class Problem_771
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        int count = 0;
+        for (int i = 0; i < jewels.Length; i++)
+        {
+            for (int j = 0; j < stones.Length; j++)
+            {
+                if (jewels[i] == stones[j]) { count++; }
+            }
+        }
+        return count;
+    }
+}
