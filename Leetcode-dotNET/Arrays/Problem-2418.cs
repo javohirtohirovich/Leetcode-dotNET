82cf01544cbe540ccd59783d8f8f6c93f246db9a
javohirtohirovich <javoergashev30@gmail.com>
2024-05-18 19:54:53 +0500

    2418 problem resolved

---
+++ Leetcode-dotNET/Arrays/Problem-2418.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode_dotNET.Arrays;
+
+public class Problem_2418
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+        var dict = new Dictionary< int,string>();
+        for(int i  = 0; i < names.Length; i++)
+        {
+            dict[heights[i]] = names[i];
+        }
+
+        heights=heights.OrderByDescending(h => h).ToArray();
+
+        var result =new string[heights.Length];
+        for(int i = 0; i < heights.Length; i++)
+        {
+            result[i] = dict[heights[i]];
+        }
+
+        return result;
+    }
+}
