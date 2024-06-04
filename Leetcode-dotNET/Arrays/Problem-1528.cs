adeff7bab46ce55f8b655a2c7850659e4ed5702a
javohirtohirovich <javoergashev30@gmail.com>
2024-06-05 01:09:23 +0500

    Problem-1528 Sloved

---
+++ Leetcode-dotNET/Arrays/Problem-1528.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_dotNET.Arrays;
+
+public class Problem_1528
+{
+    //1-Method
+   /* public string RestoreString(string s, int[] indices)
+    {
+        var dict= new Dictionary<int,char>();
+        for(int i = 0; i < indices.Length; i++)
+        {
+            dict[indices[i]] = s[i];
+        }
+        dict = dict.OrderBy(x => x.Key).ToDictionary(x=>x.Key,x=>x.Value);
+
+        var output = "";
+        foreach(int i in dict.Keys)
+        {
+            output += dict[i];
+        }
+        return output;
+    }*/
+
+
+    //2-Method
+    public string RestoreString(string s, int[] indices)
+    {
+        char[] result = new char[s.Length];
+
+        for (int i = 0; i < indices.Length; i++)
+        {
+            result[indices[i]] = s[i];
+        }
+        return new string(result);
+    }
+}
