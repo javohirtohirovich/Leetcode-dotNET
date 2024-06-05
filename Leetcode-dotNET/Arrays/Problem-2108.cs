c1218eb18ccc7b3a0cfaa1a773d97cdf05c2c933
javohirtohirovich <javoergashev30@gmail.com>
2024-06-05 22:36:53 +0500

    Problem-2108 solved

---
+++ Leetcode-dotNET/Arrays/Problem-2108.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode_dotNET.Arrays;
+
+public class Problem_2108
+{
+    public string FirstPalindrome(string[] words)
+    {
+        var result = false;
+        foreach (var word in words)
+        {
+            result = CheckWordPalindrome(word);
+            if(result != false) { return word; }
+        }
+        return "";
+    }
+    public bool CheckWordPalindrome(string word)
+    {
+        string word2 = "";
+        for(int i = word.Length - 1; i >= 0; i--)
+        {
+            word2 += word[i];
+        }
+        if(word==word2 )
+        {
+            return true;
+        }
+        else { return false; }
+    }
+}
