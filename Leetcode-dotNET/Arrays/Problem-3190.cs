5dd0941eebdc7df1915d23d294e5004dbc6b534c
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2024-07-08 22:45:36 +0500

    Sloved 3190

---
+++ Leetcode-dotNET/Arrays/Problem-3190.cs
@@ -0,0 +1,17 @@
+﻿namespace Leetcode_dotNET.Arrays;
+
+public class Problem_3190
+{
+    public int MinimumOperations(int[] nums)
+    {
+        var result = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 3 != 0)
+            {
+                result++;
+            }
+        }
+        return result;  
+    }
+}
--- Leetcode-dotNET/Program.cs
+++ Leetcode-dotNET/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var problem = new Problem_3194();
-        Console.WriteLine(problem.MinimumAverage(new int[] { 7, 8, 3, 4, 15, 13, 4, 1 }));
+        var problem = new Problem_3190();
+        Console.WriteLine(problem.MinimumOperations(new[] { 1, 2, 3, 4 } ));
     }
 }
