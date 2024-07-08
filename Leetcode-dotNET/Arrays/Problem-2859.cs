a5b46448fb0f9661771090a137857eee9c873b0e
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2024-07-08 21:38:56 +0500

    Sloved 2859

---
+++ Leetcode-dotNET/Arrays/Problem-2859.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode_dotNET.Array;
+public class Problem_2859
+{
+    public int SumIndicesWithKSetBits(IList<int> nums, int k)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (ToBinary(i) == k)
+            {
+                result += nums[i];
+            }
+        }
+        return result;
+    }
+    public static int ToBinary(int num)
+    {
+        List<int> list = new List<int>();
+        while (num > 0)
+        {
+            list.Add(num % 2);
+            num /= 2;
+        }
+
+        Console.WriteLine(list.Count(x => x == 1));
+        return list.Count(x => x == 1);
+    }
+}
