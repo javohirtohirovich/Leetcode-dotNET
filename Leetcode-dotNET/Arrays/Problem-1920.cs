ede72b224d311c2c0c83f0f572f48c4d01d1e1a2
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2024-07-08 22:27:59 +0500

    Sloved 1920

---
+++ Leetcode-dotNET/Arrays/Problem-1920.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode_dotNET.Arrays;
+
+public class Problem_1920
+{
+    public int[] BuildArray(int[] nums)
+    {
+        int[] ans = new int[nums.Length];
+        for(int i = 0;i < nums.Length; i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
