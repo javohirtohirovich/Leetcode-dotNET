678c2344a54a1914a4f35fbf9f3c56f1f3ade267
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2024-07-08 22:21:00 +0500

    slove 3065-problem

---
+++ Leetcode-dotNET/Arrays/Problem-3065.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode_dotNET.Arrays;
+
+public class Problem_3065
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int result = nums.Count(x => x < k);
+        return result;
+    }
+}
