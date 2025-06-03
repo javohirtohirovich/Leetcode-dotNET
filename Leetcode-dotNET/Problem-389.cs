3cf6379c9ff0d59257dd9352c2847f6bcc0ed3ee
javohirtohirovich <javoergashev30@gmail.com>
2025-06-03 20:00:09 +0500

    Problem 389 sloved

---
+++ Leetcode-dotNET/Problem-389.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_389
+{
+    public char FindTheDifference(string s, string t)
+    {
+        int[] letters = new int[26];
+
+        foreach (char l in s)
+        {
+            letters[l - 'a']++;
+        }
+
+        foreach (char l in t)
+        {
+            letters[l - 'a']--;
+            if (letters[l - 'a'] < 0)
+            {
+                return l;
+            }
+        }
+        
+        return ' ';
+    }
+}
