b0435e3f3b4dd9adc7fa193b00ce1aa37af7dc04
javohirtohirovich <javoergashev30@gmail.com>
2025-06-04 02:32:27 +0500

    Problem 344 sloved

---
+++ Leetcode-dotNET/Problem-0344.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_dotNET;
+
+public class Problem_0344
+{
+    public void ReverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+        while (left <= right)
+        {
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
