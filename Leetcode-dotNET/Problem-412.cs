f42a191093ee41033d2904f614310729f133ff62
javohirtohirovich <javoergashev30@gmail.com>
2025-06-03 19:23:43 +0500

    Problem 412 sloved

---
+++ Leetcode-dotNET/Problem-412.cs
@@ -0,0 +1,42 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_412
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        IList<string> result = new List<string>();
+
+        result.Add("1");
+        if(n == 1)
+        {
+            return result;
+        }
+
+        result.Add("2");
+        if (n == 2)
+        {
+            return result;
+        }
+        for (int i = 3; i <= n; i++)
+        {
+            if(i%3==0 && i % 5 == 0)
+            {
+                result.Add("FizzBuzz");
+            }
+            else if(i % 3 == 0)
+            {
+                result.Add("Fizz");
+            }
+            else if(i % 5 == 0)
+            {
+                result.Add("Buzz");
+            }
+            else
+            {
+                result.Add(i.ToString());
+            }
+        }
+
+        return result;
+    }
+}
