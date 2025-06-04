9f14a2e8ce7b9dac19592240c2a57e2024ebf01c
javohirtohirovich <javoergashev30@gmail.com>
2025-06-04 16:53:11 +0500

    Problem 383 resolved

---
+++ Leetcode-dotNET/Problem-0383.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0383
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var lettersRansomeNote = new int[26];
+        var lettersMagazine = new int[26];
+
+        foreach(var letter in ransomNote)
+        {
+            lettersRansomeNote[(int)letter - 'a']++;
+        }
+
+        foreach(var letter in magazine)
+        {
+            lettersMagazine[(int)letter - 'a']++;
+        }
+        
+        for(int i=0; i<26;i++)
+        {
+            if (lettersRansomeNote[i] > lettersMagazine[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
