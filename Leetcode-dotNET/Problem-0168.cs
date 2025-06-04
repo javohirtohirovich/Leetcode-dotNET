22c7a0f82b0dcd7c5c6aeef1938bdc4d5b71ce56
javohirtohirovich <javoergashev30@gmail.com>
2025-06-04 14:08:07 +0500

    Problem 168 sloved

---
+++ Leetcode-dotNET/Problem-0168.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_dotNET;
+
+public class Problem_0168
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        var result = "";
+        while (columnNumber > 0)
+        {
+            columnNumber--;
+            int remainder = columnNumber % 26;
+            result = (char)(remainder + 'A') + result;
+            columnNumber /= 26;
+
+        }
+        return result;
+    }
+}
