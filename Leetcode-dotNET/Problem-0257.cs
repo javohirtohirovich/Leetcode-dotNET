e08b88a0a7ffa7b206c015b41f0ef836576ca5f5
javohirtohirovich <javoergashev30@gmail.com>
2025-06-04 15:32:43 +0500

    Problem sloved

---
+++ Leetcode-dotNET/Problem-0257.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode_dotNET;
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Problem_0257
+{
+    public IList<string> BinaryTreePaths(TreeNode root)
+    {
+        var result = new List<string>();
+        if (root == null) return result;
+        if (root.left == null && root.right == null)
+        {
+            result.Add(root.val.ToString());
+            return result;
+        }
+        var leftPaths = BinaryTreePaths(root.left);
+        var rightPaths = BinaryTreePaths(root.right);
+        foreach (var path in leftPaths)
+        {
+            result.Add(root.val + "->" + path);
+        }
+        foreach (var path in rightPaths)
+        {
+            result.Add(root.val + "->" + path);
+        }
+        return result;
+    }
+}
