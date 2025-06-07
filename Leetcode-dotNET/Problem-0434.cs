6900b0ba71c3eaf33419bc0ec0e37bc8a829b382
javohirtohirovich <javoergashev30@gmail.com>
2025-06-07 22:54:30 +0500

    Problem-434 sloved

---
+++ Leetcode-dotNET/Problem-0434.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0434
+{
+    public int CountSegments(string s)
+    {
+        int count = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if ((i == 0 && !char.IsWhiteSpace(s[i])) ||
+                (i > 0 && char.IsWhiteSpace(s[i - 1]) && !char.IsWhiteSpace(s[i])))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
