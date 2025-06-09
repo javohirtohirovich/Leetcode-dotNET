052840cdad401d00bd517f203e5364cb50286e1a
javohirtohirovich <javoergashev30@gmail.com>
2025-06-09 23:31:42 +0500

    Problem sloved

---
+++ Leetcode-dotNET/Problem-0459.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0459
+{
+    public bool RepeatedSubstringPattern(string s)
+    {
+        for (int i = 1; i <= s.Length / 2; i++)
+        {
+            if (s.Length % i == 0)
+            { 
+                string part = s.Substring(0, i);
+                string repeated = string.Concat(Enumerable.Repeat(part, s.Length / i));
+
+                if (repeated == s)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
