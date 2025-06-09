5fa666cb4481b6fcf6fd68e8530482dcdac82e0a
javohirtohirovich <javoergashev30@gmail.com>
2025-06-09 17:23:09 +0500

    Not resolved Problem 500 yet

---
+++ Leetcode-dotNET/Problem-0500.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0500
+{
+    public string[] FindWords(string[] words)
+    {
+        
+        if (words.Length == 0)
+        {
+            return new string[] { };
+        }
+
+        var firstRow = new char[] { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' };
+        var secondRow = new char[] { 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' };
+        var thirdRow = new char[] { 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+
+        var result = new string[] { };
+        
+
+    }
+}
