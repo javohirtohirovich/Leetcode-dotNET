f52481d211842ca772c9cf1c4603a160e13845c3
javohirtohirovich <javoergashev30@gmail.com>
2025-06-10 15:58:42 +0500

    Added solution problem 521

---
+++ Leetcode-dotNET/Problem-0521.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0521
+{
+    public int FindLUSlength(string a, string b)
+    {
+        if (a == b) return -1;
+        if (a.Length > b.Length)
+        {
+            return a.Length;
+        }
+        return b.Length;
+    }
+}
