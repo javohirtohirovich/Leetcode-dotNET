d3fb60583e5391b0c5c062633d8844998d77f282
javohirtohirovich <javoergashev30@gmail.com>
2025-06-10 14:41:13 +0500

    Added Problem-520 solution

---
+++ Leetcode-dotNET/Problem-0520.cs
@@ -0,0 +1,43 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0520
+{
+    public bool DetectCapitalUse(string word)
+    {
+        var uppersSum = 0;
+        var lowerSum = 0;
+
+        foreach (var letter in word)
+        {
+            if (char.IsLower(letter))
+            {
+                lowerSum++;
+            }
+            else
+            {
+                uppersSum++;
+            }
+        }
+
+        if (uppersSum == 0 || lowerSum == 0)
+        {
+            return true;
+        }
+
+        if (uppersSum == 1)
+        {
+            if (char.IsUpper(word[0]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
