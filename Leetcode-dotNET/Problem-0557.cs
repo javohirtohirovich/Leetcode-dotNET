ea25e204038085910ff8bb64242d389ee4473ee2
javohirtohirovich <javoergashev30@gmail.com>
2025-06-12 00:22:49 +0500

    Problem 557 sloved

---
+++ Leetcode-dotNET/Problem-0557.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Leetcode_dotNET;
+
+public class Problem_0557
+{
+    public string ReverseWords(string s)
+    {
+        var words= s.Split(' ');
+        var result = new StringBuilder();
+
+        foreach(var word in words)
+        {
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                result.Append(word[i]);
+            }
+            result.Append(" ");
+        }
+
+        result.Remove(result.Length - 1,1);
+        return result.ToString();
+    }
+}
