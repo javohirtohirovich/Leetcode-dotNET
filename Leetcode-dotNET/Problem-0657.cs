e01870a0e31303d8b465627d506465241233ee0a
javohirtohirovich <javoergashev30@gmail.com>
2025-06-13 02:13:19 +0500

    Added solution  problem - 657

---
+++ Leetcode-dotNET/Problem-0657.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0657
+{
+    public bool JudgeCircle(string moves)
+    {
+        int x = 0;
+        int y = 0;
+
+        foreach (var move in moves)
+        {
+            switch(move)
+            {
+                case 'U': y += 1; break;
+                case 'D': y -= 1; break;
+                case 'R': x += 1; break;
+                case 'L': x -= 1; break;
+
+            }
+
+        }
+        if( x == 0 && y == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
