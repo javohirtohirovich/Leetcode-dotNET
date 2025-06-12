8330211726e15414495e3c4596e2ccccc75f3d35
javohirtohirovich <javoergashev30@gmail.com>
2025-06-13 00:34:21 +0500

    Added Problem-599 sloved

---
+++ Leetcode-dotNET/Problem-0599.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0599
+{
+    public string[] FindRestaurant(string[] list1, string[] list2)
+    {
+        var dict1 = new Dictionary<string, int>();
+        var result = new List<string>();
+
+        for (int i = 0; i < list1.Length; i++)
+        {
+            dict1[list1[i]] = i;
+        }
+
+        int minIndex = int.MaxValue;
+
+        for (int i = 0; i < list2.Length; i++)
+        {
+            if (dict1.TryGetValue(list2[i], out int index1))
+            {
+                int sumIndex = index1 + i;
+
+                if (sumIndex < minIndex)
+                {
+                    result.Clear();
+                    result.Add(list2[i]);
+                    minIndex = sumIndex;
+                }
+                else if (sumIndex == minIndex)
+                {
+                    result.Add(list2[i]);
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+
+}
--- Leetcode-dotNET/Program.cs
+++ Leetcode-dotNET/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var problem = new Problem_0551();
-        Console.WriteLine(problem.CheckRecord("PPALLL"));
+        var problem = new Problem_0599();
+        Console.WriteLine(problem.FindRestaurant(new string[] { "Shogun", "Tapioca Express", "Burger King", "KFC" }, new string[] { "KFC", "Shogun", "Burger King" }));
     }
 }
