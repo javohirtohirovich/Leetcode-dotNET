ced2a7c0b0c01e22a752daed65093fa801454fc2
javohirtohirovich <javoergashev30@gmail.com>
2025-06-15 01:49:34 +0500

    Added solution problem 796

---
+++ Leetcode-dotNET/Problem-0796.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0796
+{
+    public bool RotateString(string s, string goal)
+    {
+        if (s.Length != goal.Length) return false;
+        else if (s == goal) return true;
+
+        int i = 0;
+        while (i < goal.Length)
+        {
+            s = s.Substring(1) + s[0];
+            if (s == goal) return true;
+            i++;
+        }
+
+        return false;
+    }
+}
