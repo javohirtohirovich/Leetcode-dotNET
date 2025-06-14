3e2c531bda4cfd96df34eb6839b7996aebc8bfc5
javohirtohirovich <javoergashev30@gmail.com>
2025-06-15 02:07:29 +0500

    Added solution problem 804

---
+++ Leetcode-dotNET/Problem-0804.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0804
+{
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        var morseLetters = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        var res = new HashSet<string>();
+
+        foreach (var word in words)
+        {
+            var morse = "";
+            foreach(var l in word)
+            {
+                morse += morseLetters[l - 'a'];
+            }
+            res.Add(morse);
+        }
+
+        return res.Count;
+    }
+}
