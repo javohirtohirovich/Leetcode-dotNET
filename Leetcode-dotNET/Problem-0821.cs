f09dbf4b3e419599a2df5d2a840614f4669bf360
javohirtohirovich <javoergashev30@gmail.com>
2025-07-05 23:51:32 +0500

    Problem - 821 Added solution

---
+++ Leetcode-dotNET/Problem-0821.cs
@@ -0,0 +1,33 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0821
+{
+    public int[] ShortestToChar(string s, char c)
+    {
+        var indexes = new List<int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == c)
+            {
+                indexes.Add(i);
+            }
+        }
+
+        var result = new int[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            var minIndex = int.MaxValue;
+            foreach (int j in indexes)
+            {
+                if (Math.Abs(i - j) < minIndex)
+                {
+                    minIndex = Math.Abs(i - j);
+                }
+
+            }
+            result[i] = minIndex;
+        }
+
+        return result;
+    }
+}
