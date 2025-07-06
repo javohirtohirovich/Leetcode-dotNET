f9f11f63f9a499ece079adffd74674e5a909df2b
javohirtohirovich <javoergashev30@gmail.com>
2025-07-06 23:46:32 +0500

    Problem - 831 added solution

---
+++ Leetcode-dotNET/Problem-0831.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0831
+{
+    public IList<IList<int>> LargeGroupPositions(string s)
+    {
+        var result = new List<IList<int>>();
+
+        var counter = 1;
+        var letter = s[0];
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (letter == s[i])
+            {
+                counter++;
+                letter = s[i];
+            }
+            else
+            {
+                if (counter > 2)
+                {
+                    result.Add(new List<int>() { i - counter, i-1 });
+                }
+                letter = s[i];
+                counter = 1;
+            }
+        }
+
+        if (counter > 2)
+        {
+            result.Add(new List<int>() { s.Length - counter, s.Length - 1 });
+        }
+
+        return result;
+    }
+}
