04c9f2ff472f615977c5a4ed02c6583f5e229b32
javohirtohirovich <javoergashev30@gmail.com>
2025-07-13 18:50:46 +0500

    Problem resolved

---
+++ Leetcode-dotNET/Problem-0696.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0696
+{
+    public int CountBinarySubstrings(string s)
+    {
+        int prev = 0, curr = 1, result = 0;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                curr++;
+            }
+            else
+            {
+                result += Math.Min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+
+        result += Math.Min(prev, curr);
+        return result;
+    }
+
+}
