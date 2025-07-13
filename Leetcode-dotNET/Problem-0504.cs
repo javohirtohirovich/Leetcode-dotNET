900ab8b481b2dee82fcd5d6b4c579fb1264abf95
javohirtohirovich <javoergashev30@gmail.com>
2025-07-13 19:11:55 +0500

    Added solution problem 504

---
+++ Leetcode-dotNET/Problem-0504.cs
@@ -0,0 +1,21 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0504
+{
+    public string ConvertToBase7(int num)
+    {
+        var result = "";
+        var n = Math.Abs(num);
+        while (n != 0)
+        {
+            var remainder = n % 7;
+            result = remainder + result;
+            n /= 7;
+        }
+        if (num < 0)
+        {
+            result = "-" + result;
+        }
+        return result == "" ? "0" : result;
+    }
+}
