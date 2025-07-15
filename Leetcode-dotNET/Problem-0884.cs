6bb0edcc10e8993e8dd108a40fef9e906e7b8809
javohirtohirovich <javoergashev30@gmail.com>
2025-07-16 01:08:00 +0500

    added solutions

---
+++ Leetcode-dotNET/Problem-0884.cs
@@ -0,0 +1,44 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0884
+{
+    public string[] UncommonFromSentences(string s1, string s2)
+    {
+        var s1Arr = s1.Split(" ");
+        var s2Arr = s2.Split(" ");
+        var hashSet = new HashSet<string>();
+        foreach (var i in s1Arr)
+        {
+            hashSet.Add(i);
+        }
+        foreach (var i in s2Arr)
+        {
+            hashSet.Add(i);
+        }
+
+        var dict = new Dictionary<string, int>();
+        foreach (var i in hashSet)
+        {
+            dict[i] = 0;
+        }
+
+        var result = new List<string>();
+        foreach(var i in s1Arr)
+        {
+            dict[i]++;
+        }
+        foreach (var i in s2Arr)
+        {
+            dict[i]++;
+        }
+
+        foreach(KeyValuePair<string, int> pair in dict)
+        {
+            if (pair.Value == 1)
+            {
+                result.Add(pair.Key);
+            }
+        }
+        return result.ToArray();
+    }
+}
---
+++ Leetcode-dotNET/Problem-0917.cs
@@ -0,0 +1,68 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0917
+{
+    public string ReverseOnlyLetters(string s)
+    {
+        var sArr = new char[s.Length];
+        var charCount = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!char.IsLetter(s[i]))
+            {
+                sArr[i] = s[i];
+                charCount++;
+            }
+        }
+
+        var lettersCount = s.Length - charCount;
+        var j = s.Length - 1;
+        var f = 0;
+        while (lettersCount > 0)
+        {
+            if (sArr[f] != '\0')
+            {
+                f++;
+            }
+            else if (!char.IsLetter(s[j]))
+            {
+                j--;
+            }
+            else if (char.IsLetter(s[j]) && sArr[f] == '\0')
+            {
+                sArr[f] = s[j];
+                f++;
+                j--;
+                lettersCount--;
+            }
+           
+        }
+        return new string(sArr);
+    }
+
+    //Sample solution
+    //public string ReverseOnlyLetters(string s)
+    //{
+    //    char[] arr = s.ToCharArray();
+    //    int left = 0, right = s.Length - 1;
+
+    //    while (left < right)
+    //    {
+    //        if (!char.IsLetter(arr[left]))
+    //        {
+    //            left++;
+    //        }
+    //        else if (!char.IsLetter(arr[right]))
+    //        {
+    //            right--;
+    //        }
+    //        else
+    //        {
+    //            (arr[left], arr[right]) = (arr[right], arr[left]);
+    //            left++;
+    //            right--;
+    //        }
+    //    }
+    //    return new string(arr);
+    //}
+}
