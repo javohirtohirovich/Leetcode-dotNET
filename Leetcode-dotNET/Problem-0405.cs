118d49d749d829566521fb286c202c6f6a05eaa8
javohirtohirovich <javoergashev30@gmail.com>
2025-10-09 00:22:32 +0500

    Added 405 problem solution

---
+++ Leetcode-dotNET/Problem-0405.cs
@@ -0,0 +1,29 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0405
+{
+    public string ToHex(int num)
+    {
+        if (num == 0) return "0";
+
+        uint n = (uint)num;
+
+        string result = "";
+        while (n != 0)
+        {
+            uint remainder = n % 16;
+            if (remainder < 10)
+            {
+                result = (char)(remainder + '0') + result;
+            }
+            else
+            {
+                result = (char)(remainder - 10 + 'a') + result;
+            }
+
+            n /= 16;
+        }
+
+        return result;
+    }
+}
