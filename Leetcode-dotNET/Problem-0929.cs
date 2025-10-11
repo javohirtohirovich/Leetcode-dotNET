5352a678baefcdc10c58cfdc6b1dd985aeb2733a
javohirtohirovich <javoergashev30@gmail.com>
2025-10-12 01:40:49 +0500

    Added solution 929 problem

---
+++ Leetcode-dotNET/Problem-0929.cs
@@ -0,0 +1,76 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0929
+{
+
+    public int NumUniqueEmails(string[] emails)
+    {
+        var result = new HashSet<string>();
+        foreach (var email in emails)
+        {
+            var domainIndex = email.IndexOf('@');
+            var plusIndex = email.IndexOf('+');
+
+            var localName = "";
+            if (plusIndex == -1)
+            {
+                localName = email.Substring(0, domainIndex).Replace(".", "");
+            }
+            else
+            {
+                localName = email.Substring(0, plusIndex).Replace(".", "");
+            }
+
+            var domainName = email.Substring(domainIndex);
+
+            result.Add(localName +domainName);
+        }
+
+        return result.Count;
+    }
+
+    // solution 1 - 20 ms Beats 9.87%
+
+    /*public int NumUniqueEmails(string[] emails)
+    {
+        var result = new HashSet<string>();
+        foreach (var email in emails)
+        {
+            result.Add(ClearEmail(email));
+        }
+
+        return result.Count;
+    }
+
+    public string ClearEmail(string email)
+    {
+        var result = "";
+        var f = true;
+        var a = false;
+
+        foreach (var i in email)
+        {
+            
+            if (i == '.' && !a )
+            {
+                continue;
+            }
+            else if (i == '+'&&!a)
+            {
+                f = false;
+            }
+            else if (i == '@')
+            {
+                result += i;
+                f = true;
+                a = true;
+            }
+            else if (f)
+            {
+                result += i;
+            }
+        }
+
+        return result;
+    }*/
+}
--- Leetcode-dotNET/Program.cs
+++ Leetcode-dotNET/Program.cs
@@ -5,7 +5,11 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var problem = new Problem_0844();
-        Console.WriteLine(problem.BackspaceCompare("xywrrmp", "xywrrmu#p"));
+        var problem = new Problem_0929();
+        Console.WriteLine(problem.NumUniqueEmails(
+            new string[] {
+               "a@e+c.com", "a@e+c+f.com"
+            }
+        ));
     }
 }
