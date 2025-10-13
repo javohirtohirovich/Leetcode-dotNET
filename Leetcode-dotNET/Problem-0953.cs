ddd41652b0b1050c00186257b2c4cfb2027c0855
javohirtohirovich <javoergashev30@gmail.com>
2025-10-13 19:34:11 +0500

    WIP

---
+++ Leetcode-dotNET/Problem-0953.cs
@@ -0,0 +1,40 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0953
+{
+    public bool IsAlienSorted(string[] words, string order)
+    {
+        var s = words[0];
+        for (int i = 1; i < words.Length; i++)
+        {
+            var r = 0;
+            if (s.Length < words[i].Length)
+            {
+                r = s.Length;
+            }
+            else
+            {
+                r = words[i].Length;
+            }
+            for (int j = 0; j < r; j++)
+            {
+                if (order.IndexOf(s[j]) > order.IndexOf(words[i][j]))
+                {
+                    return false;
+                }
+                else if (order.IndexOf(s[j]) < order.IndexOf(words[i][j]))
+                {
+                    break;
+                }
+                if(j == r - 1 && s.Length > words[i].Length)
+                {
+                    return false;
+                }
+               
+            }
+            s = words[i];
+        }
+
+        return true;
+    }
+}
