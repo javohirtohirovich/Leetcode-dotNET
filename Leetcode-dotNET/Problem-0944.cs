af5b94e1e3446f58649ddd57513d9c78e936b35a
javohirtohirovich <javoergashev30@gmail.com>
2025-10-13 16:36:49 +0500

    WIP

---
+++ Leetcode-dotNET/Problem-0944.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0944
+{
+    public int MinDeletionSize(string[] strs)
+    {
+        var notSortedColumsCount = 0;
+
+        for(int i = 0; i < strs[0].Length; i++)
+        {
+            for(int j = 1; j<strs.Length; j++)
+            {
+                if (strs[j][i] < strs[j - 1][i])
+                {
+                    notSortedColumsCount++;
+                    break;
+                }
+            }
+        }
+
+        return notSortedColumsCount;
+    }
+}
