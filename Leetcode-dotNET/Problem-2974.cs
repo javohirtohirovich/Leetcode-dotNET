1851be93ae00d5263374f2dd9e34e23b16a181ae
javohirtohirovich <javoergashev30@gmail.com>
2025-10-19 15:58:15 +0500

    Added solution problem 2874

---
+++ Leetcode-dotNET/Problem-2974.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_2974
+{
+    public int[] NumberGame(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for(int i = 0; i < nums.Length; i += 2)
+        {
+            int j = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = j;
+        }
+
+        return nums;
+    }
+}
