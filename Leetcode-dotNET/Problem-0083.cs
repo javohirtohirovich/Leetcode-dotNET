2bc2deeae75fc0daab09ff684a738d2ee3acc86a
javohirtohirovich <javoergashev30@gmail.com>
2025-12-17 22:51:10 +0500

    Added Soultions

---
+++ Leetcode-dotNET/Problem-0083.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0083
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head == null || head.next == null) return head;
+
+        var current = head;
+        while(current != null && current.next != null)
+        {
+            if(current.val == current.next.val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+
+        }
+        return head;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ Leetcode-dotNET/Problem-1832.cs
@@ -0,0 +1,32 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_1832
+{
+
+    public bool CheckIfPangram(string sentence)
+    {
+        var alph = "abcdefghijklmnopqrstuvwxyz";
+
+        for(int i = 0;  i < alph.Length; i++)
+        {
+            if(!sentence.Contains(alph[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //solution - 1
+    //public bool CheckIfPangram(string sentence)
+    //{
+    //    var result = new HashSet<char>();
+    //    foreach(char c in sentence)
+    //    {
+    //        result.Add(c);
+    //    }
+
+    //    return result.Count == 26;
+    //}
+}
