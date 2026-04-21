d50579733a4abf7c30b6369c460635e3b259ba63
javohirtohirovich <j.ergashev@mbabm.uz>
2026-04-21 17:15:38 +0500

    Added new problem solutions

---
+++ Leetcode-dotNET/Problem-0141.cs
@@ -0,0 +1,32 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0141
+{
+    public bool HasCycle(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+}
+
---
+++ Leetcode-dotNET/Problem-0160.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0160
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        var pA = headA;
+        var pB = headB;
+        while (pA != pB)
+        {
+            if (pA == null)
+            {
+                pA = headB;
+            }
+            else
+            {
+                pA = pA.next;
+            }
+            if (pB == null)
+            {
+                pB = headA;
+            }
+            else
+            {
+                pB = pB.next;
+            }
+        }
+        return pA;
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+}
