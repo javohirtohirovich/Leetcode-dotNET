cc84f63a469f1bb876bb2971773803ee8e50d754
javohirtohirovich <j.ergashev@mbabm.uz>
2026-04-21 17:48:49 +0500

    Added solution problem 203

---
+++ Leetcode-dotNET/Problem-0203.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0203
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        if (head == null)
+        {
+            return head;
+        }
+
+        while (head != null && head.val == val)
+        {
+            head = head.next;
+        }
+        var current = head;
+        while (current != null && current.next != null)
+        {
+            if (current.next.val == val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
