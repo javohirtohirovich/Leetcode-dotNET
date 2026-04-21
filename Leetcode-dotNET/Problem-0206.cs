03285b38e63dae8b1255d44132dc92c0c48e8687
javohirtohirovich <j.ergashev@mbabm.uz>
2026-04-21 18:00:05 +0500

    Added solution problem - 206

---
+++ Leetcode-dotNET/Problem-0206.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0206
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        var current = head;
+
+        while (current != null)
+        {
+            var next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
