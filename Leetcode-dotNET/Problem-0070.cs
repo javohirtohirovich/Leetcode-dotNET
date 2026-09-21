687919fe4b68bd5c2ca145b85a43728119a74b0e
Javohir Ergashev <javoergashev@outlook.com>
2026-09-21 11:47:31 +0500

    Added solution problem 70

---
+++ Leetcode-dotNET/Problem-0070.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode_dotNET;
+
+public class Problem_0070
+{
+    public int ClimbStairs(int n)
+    {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+
+        int a = 1;
+        int b = 2;
+        for(int i = 3; i <= n; i++)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+}
