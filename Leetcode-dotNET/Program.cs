0c137203bb6cc1175964cc0d411d7aa35c9ed22b
Javohir Ergashev <javoergashev@outlook.com>
2026-09-21 15:42:47 +0500

    Added solution problem-88

--- Leetcode-dotNET/Program.cs
+++ Leetcode-dotNET/Program.cs
@@ -5,7 +5,15 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var problem = new Problem_0069();
-        Console.WriteLine(problem.MySqrt(8));
+        var problem = new Problem_0088();
+
+        problem.Merge(
+            new int[] { 0 },
+            0,
+            new int[] { 1 },
+            1
+        );
+
+
     }
 }
